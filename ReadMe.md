525b6d4aefcdef018d00fbb81473641ac9fa1d2c
ahmed shaheen <ahmedshaheen93@yahoo.com>
2021-12-30 16:49:50 +0200

    create internal network, attach the container to created network

--- ReadMe.md
+++ ReadMe.md
@@ -8,8 +8,8 @@
 - start the spring boot application
 
 ## run.sh file commands
-
+- create internal network to communicate internally between the containers
 - build image
-- run a container form that image
+- run a container form that image and specify network and container name 
 - wait 20 second till the container start
 - send a curl request to test the running container
--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
+## define variable for container, network and image names
+containerName=my-application;
+imageName=my-application;
+networkName=my-network;
+
+## build the application jar
+mvn clean package;
 ## build image
-docker image build -t dockerfileexample . ;
+docker image build -t $imageName . ;
+## create internal network with name my-network
+docker network create $networkName;
+## stop running container if exists
+docker container stop $containerName;
 ## run the container
-docker container run -d -p 8080:8080 dockerfileexample;
+docker container run -d -p 8080:8080 --network $networkName --name $containerName --rm $imageName;
 ## wait 20 second until the container is loaded
 sleep 20;
 ## test running
 curl -X GET http://localhost:8080/hello/ahmed
+## list running containers
+docker container ls;

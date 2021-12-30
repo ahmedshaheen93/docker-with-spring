9dd9b0d2ed58995188e6ec83b25a3c7c1bd79a52
ahmed shaheen <ahmedshaheen93@yahoo.com>
2021-12-30 16:58:19 +0200

    update bash script file, extract and define all needed variables

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
-## define variable for container, network and image names
+## define variable for container, network and image names and all needed variables
 containerName=my-application;
 imageName=my-application;
 networkName=my-network;
-
+internalPort=8080;
+externalPort=8080;
+host=http://localhost;
+endPoint=/hello/ahmed;
+request=$host:$externalPort$endPoint
 ## build the application jar
 mvn clean package;
 ## build image
@@ -12,10 +16,10 @@ docker network create $networkName;
 ## stop running container if exists
 docker container stop $containerName;
 ## run the container
-docker container run -d -p 8080:8080 --network $networkName --name $containerName --rm $imageName;
+docker container run -d -p $externalPort:$internalPort --network $networkName --name $containerName --rm $imageName;
 ## wait 20 second until the container is loaded
 sleep 20;
-## test running
-curl -X GET http://localhost:8080/hello/ahmed
 ## list running containers
 docker container ls;
+## test running
+curl -X GET $request;
